d425cb6d193257371b256fb0442658f234146f2e
Michał Sznajder <m.sznajder@mars-edv.de>
2024-02-23 11:33:14 +0100

    Initial Commit

---
+++ dev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dev
+//
+//  Created by Michał Sznajder on 23/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
